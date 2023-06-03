fd45dc37965e349fb87553efa6d2c7e4a792a761
Hanna Kompaniiets <anna.komp24@gmail.com>
2023-06-03 14:26:38 +0200

    1task begin

--- src/service/image-service.js
+++ src/service/image-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '';
+const API_KEY = 'LqdMyZMOt2jhKLy7gSpMcnkKi2ysGicFxR641v833ReVTacqB15tsu02';
 axios.defaults.baseURL = 'https://api.pexels.com/v1/';
 axios.defaults.headers.common['Authorization'] = API_KEY;
 axios.defaults.params = {
@@ -8,4 +8,10 @@ axios.defaults.params = {
   per_page: 15,
 };
 
-export const getImages = (query, page) => {};
+export const getImages = async (query, page) => {
+
+  const { data } = await axios.get(`search?query=${query}&page=${page}`) 
+  return data
+};
+
+
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -1,12 +1,19 @@
 import { Component } from 'react';
 
+
 import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+
+
+  onHandleSubmit = (value) => { 
+    console.log(value, 'console from Gallery')
+  }
   render() {
     return (
       <>
+        <SearchForm onSubmit={this.onHandleSubmit} />
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
