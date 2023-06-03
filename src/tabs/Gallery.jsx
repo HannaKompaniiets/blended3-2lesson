8b70f9ca5b9ec6d4f54421f31ef80332d270355d
MariySemenenko <mashyni92@gmail.com>
2023-06-03 16:13:38 +0300

    task 2

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -1,15 +1,54 @@
 import { Component } from 'react';
 
-
 import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    images: [],
+    query: '',
+    page: 1,
+    isLoading: false,
+    isEmpty: false,
+    isVisible: false,
+  };
 
-
-  onHandleSubmit = (value) => { 
-    console.log(value, 'console from Gallery')
+  componentDidUpdate(_, prevState) {
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      this.getPhotos(query, page);
+    }
   }
+
+  getPhotos = async (query, page) => {
+    if (!query) {
+      return;
+    }
+
+    this.setState({ isLoading: true });
+
+    try {
+      const {
+        photos,
+        page: currentPage,
+        total_results,
+        per_page,
+      } = await ImageService.getImages(query, page);
+      if (photos.length === 0) {
+        this.setState({ isEmpty: true });
+      }
+      this.setState(prevState => ({
+        images: [...prevState.images, ...photos],
+        isVisible: currentPage < Math.ceil(total_results / per_page),
+      }));
+    } catch (error) {
+    } finally {
+    }
+  };
+
+  onHandleSubmit = value => {
+    console.log(value, 'console from Gallery');
+  };
   render() {
     return (
       <>
